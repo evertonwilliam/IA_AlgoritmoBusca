7c4e8c447bb7328b49f04fef8bb918a61058599e
Everton William <evertonwilliam@gmail.com>
2022-07-29 16:49:15 -0300

    Versão 0.0.1

---
+++ IA_AlgoritmoBusca/model/Adjacente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA_AlgoritmoBusca.model
+{
+    internal class Adjacente
+    {
+        public int IdAdjacente { get; set; }
+        public int IdCidadeOrigem { get; set; }
+        public int IdCidadeDestino { get; set; }
+        public int DistRodEntreCidades { get; set; }
+        public Adjacente()
+        {
+
+        }
+    }
+}
---
+++ IA_AlgoritmoBusca/model/Cidade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA_AlgoritmoBusca.model
+{
+    internal class Cidade
+    {
+        public int IdCidade { get; set; }
+        public string NomeCidade { get; set; }
+        public Cidade()
+        {
+
+        }
+    }
+}
