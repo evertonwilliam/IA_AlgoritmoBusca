1d3ffb355a6f5fa546812abf83d661cd2dca1ff2
Everton William <evertonwilliam@gmail.com>
2022-07-29 17:28:01 -0300

    Versão 0.0.1

--- IA_AlgoritmoBusca/FrmPrincipal.Designer.cs
+++ IA_AlgoritmoBusca/FrmPrincipal.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.BtLoad = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.BtFila = new System.Windows.Forms.Button();
+            this.BtPilha = new System.Windows.Forms.Button();
+            this.BtProfundidade = new System.Windows.Forms.Button();
+            this.BtGuloso = new System.Windows.Forms.Button();
+            this.BtLargura = new System.Windows.Forms.Button();
+            this.BtAEstrela = new System.Windows.Forms.Button();
+            this.GbFila = new System.Windows.Forms.GroupBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,20 +53,97 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.BtAEstrela);
+            this.panel1.Controls.Add(this.BtLargura);
+            this.panel1.Controls.Add(this.BtGuloso);
+            this.panel1.Controls.Add(this.BtProfundidade);
+            this.panel1.Controls.Add(this.BtPilha);
+            this.panel1.Controls.Add(this.BtFila);
             this.panel1.Controls.Add(this.BtLoad);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(820, 86);
+            this.panel1.Size = new System.Drawing.Size(640, 85);
             this.panel1.TabIndex = 1;
+            // 
+            // BtFila
+            // 
+            this.BtFila.Enabled = false;
+            this.BtFila.Location = new System.Drawing.Point(92, 12);
+            this.BtFila.Name = "BtFila";
+            this.BtFila.Size = new System.Drawing.Size(81, 61);
+            this.BtFila.TabIndex = 1;
+            this.BtFila.Text = "Fila";
+            this.BtFila.UseVisualStyleBackColor = true;
+            // 
+            // BtPilha
+            // 
+            this.BtPilha.Enabled = false;
+            this.BtPilha.Location = new System.Drawing.Point(172, 12);
+            this.BtPilha.Name = "BtPilha";
+            this.BtPilha.Size = new System.Drawing.Size(81, 61);
+            this.BtPilha.TabIndex = 2;
+            this.BtPilha.Text = "Pilha";
+            this.BtPilha.UseVisualStyleBackColor = true;
+            // 
+            // BtProfundidade
+            // 
+            this.BtProfundidade.Enabled = false;
+            this.BtProfundidade.Location = new System.Drawing.Point(305, 13);
+            this.BtProfundidade.Name = "BtProfundidade";
+            this.BtProfundidade.Size = new System.Drawing.Size(81, 61);
+            this.BtProfundidade.TabIndex = 3;
+            this.BtProfundidade.Text = "Profundo";
+            this.BtProfundidade.UseVisualStyleBackColor = true;
+            // 
+            // BtGuloso
+            // 
+            this.BtGuloso.Enabled = false;
+            this.BtGuloso.Location = new System.Drawing.Point(467, 12);
+            this.BtGuloso.Name = "BtGuloso";
+            this.BtGuloso.Size = new System.Drawing.Size(81, 61);
+            this.BtGuloso.TabIndex = 4;
+            this.BtGuloso.Text = "Guloso";
+            this.BtGuloso.UseVisualStyleBackColor = true;
+            // 
+            // BtLargura
+            // 
+            this.BtLargura.Enabled = false;
+            this.BtLargura.Location = new System.Drawing.Point(386, 13);
+            this.BtLargura.Name = "BtLargura";
+            this.BtLargura.Size = new System.Drawing.Size(81, 61);
+            this.BtLargura.TabIndex = 5;
+            this.BtLargura.Text = "Largura";
+            this.BtLargura.UseVisualStyleBackColor = true;
+            // 
+            // BtAEstrela
+            // 
+            this.BtAEstrela.Enabled = false;
+            this.BtAEstrela.Location = new System.Drawing.Point(548, 12);
+            this.BtAEstrela.Name = "BtAEstrela";
+            this.BtAEstrela.Size = new System.Drawing.Size(81, 61);
+            this.BtAEstrela.TabIndex = 6;
+            this.BtAEstrela.Text = "A*";
+            this.BtAEstrela.UseVisualStyleBackColor = true;
+            // 
+            // GbFila
+            // 
+            this.GbFila.Location = new System.Drawing.Point(0, 91);
+            this.GbFila.Name = "GbFila";
+            this.GbFila.Size = new System.Drawing.Size(820, 68);
+            this.GbFila.TabIndex = 3;
+            this.GbFila.TabStop = false;
+            this.GbFila.Text = "Processando a fila";
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(820, 332);
+            this.ClientSize = new System.Drawing.Size(640, 357);
+            this.Controls.Add(this.GbFila);
             this.Controls.Add(this.panel1);
             this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Estudo dos Algorítmos de Busca";
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -70,6 +154,13 @@
 
         private System.Windows.Forms.Button BtLoad;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtAEstrela;
+        private System.Windows.Forms.Button BtLargura;
+        private System.Windows.Forms.Button BtGuloso;
+        private System.Windows.Forms.Button BtProfundidade;
+        private System.Windows.Forms.Button BtPilha;
+        private System.Windows.Forms.Button BtFila;
+        private System.Windows.Forms.GroupBox GbFila;
     }
 }
 
